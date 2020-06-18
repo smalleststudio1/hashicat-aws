982e5cb2d8aa3454eaab5b93d7cebd59ee53da59
Your Name <you@example.com>
2020-06-18 08:26:32 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,6 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
