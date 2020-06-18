292859647397abd30b72e3dde8cf09097232513e
Your Name <you@example.com>
2020-06-18 09:21:35 +0000

    add module basic

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,5 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  create_vpc = false
-  # ... omitted
+  source  = "app.terraform.io/juyi-training/vpc/aws"
+  version = "2.39.0"
+  cidr = "10.0.0.0/16"
 }
